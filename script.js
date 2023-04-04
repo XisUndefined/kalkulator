fcc50d197ce3e47cd9998cbb6d5a4fcead2e4ed5
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2023-04-04 17:20:44 +0700

    make the percentage operator works

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const screen = document.querySelector('.screen');
 const clearBtn = document.querySelector('.all-clear');
 const equalSign = document.querySelector('.equal-sign');
 const decimal = document.querySelector('.decimal');
+const percentage = document.querySelector('.percentage');
 
 const clearAll = () => {
     prevNumber = '';
@@ -65,6 +66,10 @@ decimal.addEventListener('click', (event) => {
     screenUpdate(currentNumber);
 });
 
+percentage.addEventListener('click', (event) => {
+    inputOperation(event.target.value);
+});
+
 const calculate = () => {
     let result = '';
     switch(calcOperation) {
@@ -79,6 +84,9 @@ const calculate = () => {
             break;
         case '/':
             result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        case '%':
+            result = parseFloat(prevNumber) / 100;
             break;
         default:
             return;
