9b775efe3b55beb1643adcab59cbe2ef339a05ad
XisUndefined <muhlisahmadabdillah_1306619028@mhs.unj.ac.id>
2023-04-06 01:34:44 +0700

    insya allah final

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const inputNumber = (number) => {
     if (display === '0') {
         display = number;
         prompt = number;
-    } else if (result !== '' && ['+', '-', '/', '*', '.'].includes(prompt.slice(-1)) === false) {
+    } else if (result !== '') {
         result = '';
         display = number;
         prompt = number;
+    } else if (prompt.slice(-4) === '/100') {
+        prompt += '';
+        display += '';
     } else {
         display += number;
         prompt += number;
@@ -26,6 +29,7 @@ const inputOperator = (oprPrompt, oprDisplay) => {
         display = display.slice(0, -1);
         display += oprDisplay;
     } else {
+        result = '';
         prompt += oprPrompt;
         display += oprDisplay;
     };
@@ -42,26 +46,55 @@ const screenUpdate = (number) => {
 };
 
 const calcResult = (number) => {
-    result = eval(number);
-    prompt = result.toString();
-    display = result.toString();
+    if (['+', '-', '/', '*'].some(opr => prompt.includes(opr))) {
+        result = eval(number);
+        prompt = result.toString();
+        display = result.toString();
+    }
 };
 
 keys.addEventListener('click', (event) => {
     if (event.target.className.includes('number')) {
         inputNumber(event.target.value);
         screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
     } else if (event.target.className == 'operator') {
         inputOperator(event.target.value, event.target.innerHTML);
         screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
     } else if (event.target.className == 'all-clear') {
         clearAll();
         screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
     } else if (event.target.className == 'percentage') {
         inputOperator('/100', event.target.value);
         screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
+    } else if (event.target.className == 'decimal') {
+        inputOperator(event.target.value, event.target.innerHTML);
+        screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
     } else {
         calcResult(prompt);
         screenUpdate(display);
+        console.log(`display\t: ${display}`);
+        console.log(`prompt\t: ${prompt}`);
+        console.log(`result\t: ${result}`);
+        console.log(``);
     };
 });
